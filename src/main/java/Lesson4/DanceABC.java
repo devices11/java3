dd7f29aa9cd43f2ed890c2534a50b94ce71a905e
Valentin Izotov <devices11@gmail.com>
2020-02-08 18:52:04 +0300

    add dz Lesson4

---
+++ src/main/java/Lesson4/DanceABC.java
@@ -0,0 +1,83 @@
+package Lesson4;
+
+public class DanceABC {
+    //1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+
+    public static void main(String[] args) {
+        DanceABC danceABC = new DanceABC();
+
+        Thread t1 = new Thread(() -> {
+            danceABC.printA();
+        });
+
+        Thread t2 = new Thread(() -> {
+            danceABC.printB();
+        });
+
+        Thread t3 = new Thread(() -> {
+            danceABC.printC();
+        });
+
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
